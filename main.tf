5ad28d1c72928de01412f98ec8215ce5412b4d8d
Ahmad <sohail.yu@gmail.com>
2020-12-31 20:58:30 -0700

    adding vpc-id

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ data "aws_vpc" "test" {
 # Create Security Group for EC2
 resource "aws_security_group" "default" {
   name = "terraform-default-sg"
+  vpc_id = data.aws_vpc.test.id
 
   ingress {
     from_port   = 80
